839862fd6edbed383bc924abbb3c11b36e5249c7
EbichanOO <takanori.sao@icloud.com>
2019-10-11 03:04:35 +0900

    [fix]port

--- server.py
+++ server.py
@@ -53,11 +53,6 @@ def menu():
 
 if __name__ == "__main__":
   import os
-  ON_HEROKU = os.environ.get('ON_HEROKU')
-
-  if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-  else:
-    port = 3000
+  # get the heroku port
+  port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
   app.run(host='0.0.0.0', port=port, debug=True)
